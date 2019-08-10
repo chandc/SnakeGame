fa83c9de05e84ea49e92d40401006e5365d41eb4
chandc <chandc999@gmail.com>
2019-08-10 19:01:41 -0400

    build a function to play sound

--- src/game.cpp
+++ src/game.cpp
@@ -3,6 +3,32 @@
 #include "SDL.h"
 #include <SDL_mixer.h>
 
+
+void PlayMusic()
+{
+  Mix_Music *sound = NULL;
+
+  int channel;
+  int audio_rate = 22050;
+  Uint16 audio_format = AUDIO_S16SYS;
+  int audio_channels = 2;
+  int audio_buffers = 4096;
+    if (SDL_Init(SDL_INIT_AUDIO) != 0) {
+                printf("Unable to initialize SDL: %s\n", SDL_GetError());
+                }
+    if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) != 0) {
+                printf("Unable to initialize audio: %s\n", Mix_GetError());
+        }
+    sound = Mix_LoadMUS("./beat.wav");
+
+    if(sound == NULL) {
+                printf("Unable to load WAV file: %s\n", Mix_GetError());
+        }
+    channel = Mix_PlayMusic( sound, -1 );
+      if(channel == -1) {
+                printf("Unable to play WAV file: %s\n", Mix_GetError());
+        }
+}
 Game::Game(std::size_t grid_width, std::size_t grid_height)
     : snake(grid_width, grid_height),
       engine(dev()),
@@ -20,33 +46,7 @@ void Game::Run(Controller const &controller, Renderer &renderer,
   int frame_count = 0;
   bool running = true;
 
-// play sound
-//Mix_Chunk *sound = NULL;
-
-  Mix_Music *sound = NULL;
-
-  int channel;
-  int audio_rate = 22050;
-  Uint16 audio_format = AUDIO_S16SYS;
-  int audio_channels = 2;
-  int audio_buffers = 4096;
-    if (SDL_Init(SDL_INIT_AUDIO) != 0) {
-                printf("Unable to initialize SDL: %s\n", SDL_GetError());
-                }
-    if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) != 0) {
-                printf("Unable to initialize audio: %s\n", Mix_GetError());
-        }
-    //sound = Mix_LoadWAV("beat.wav");
-    sound = Mix_LoadMUS("beat.wav");
-
-    if(sound == NULL) {
-                printf("Unable to load WAV file: %s\n", Mix_GetError());
-        }
-    //channel = Mix_PlayChannel(-1, sound, 0);
-    channel = Mix_PlayMusic( sound, -1 );
-      if(channel == -1) {
-                printf("Unable to play WAV file: %s\n", Mix_GetError());
-        }
+  PlayMusic();
 
   while (running) {
     frame_start = SDL_GetTicks();
